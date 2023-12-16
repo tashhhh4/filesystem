2c8811bcff23458747f408e8ef04931c024ad69c
Natasha Libera <tash@artoftash.com>
2023-12-16 15:07:51 +0100

    Styled the login page.

--- app/login.php
+++ app/login.php
@@ -30,17 +30,22 @@ if(isset($username) AND isset($password)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
     <title>File System - Please Log In</title>
 </head>
 <body>
+ <div class="form-container">
   <form class="login-form" action="login.php" method="post">
-    <p class="login-error"><?php echo $loginError; ?></p>
+   <p class="login-error"><?php echo $loginError; ?></p>
+   <div class="form-content">
     <p class="login-prompt">Please log in to access the file system.</p>
     <label for="username_input">Username:</label>
     <input id="username_input" name="username" type="text" required>
     <label for="password_input">Password:</label>
     <input id="password_input" name="password" type="password" required>
     <input type="submit" value="Login">
+   </div>
   </form>
+ </div>
 </body>
 </html>
