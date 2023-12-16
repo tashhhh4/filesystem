1558c336995f904e7c2a03c3f106db871d2989f3
Natasha Libera <tash@artoftash.com>
2023-12-16 13:42:30 +0100

    Added login form.

--- app/index.php
+++ app/index.php
@@ -1,1 +1,17 @@
+<?php 
+// Check Auth
+$auth = false;
+if(isset($_COOKIE['auth'])) {
+    $auth = ($_COOKIE['auth'] === 'authorized!');
+}
+if ($auth) {
+    $user = $_COOKIE['user'];       
+} else {
+    // Redirect to login page
+    header('Location: login.php');
+}
+?>
+
 Hello this is the Index Page
+
+You need to be logged in to see this stuff.
--- app/login.php
+++ app/login.php
@@ -1,1 +1,21 @@
-Here is the login page.
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File System - Please Log In</title>
+</head>
+<body>
+  <form class="login-form" action="login.php" method="post">
+    <p>Please log in to access the file system.</p>
+    <label for="username_input">Username:</label>
+    <input id="username_input" type="text" required>
+    <label for="password_input">Password:</label>
+    <input id="username_input" type="password" required>
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
